463b8a0c27e26ac9bf2b0d329c5c9979a0238e67
igormanfroi-cpu <igor_manfroi@estudante.sesisenai.org.br>
2026-02-25 20:17:38 +0000

    update

---
+++ atividade python/atividade5.py
@@ -0,0 +1,7 @@
+nome = input("insira o nome: ")
+idade = int(input("insira a idade: "))
+
+#repete quando a idade for valida
+while idade > 120 or idade < 0:
+print = int(input("idade(anos comletos - ate 120 anos): "))
+dias_vida = idade * 365
