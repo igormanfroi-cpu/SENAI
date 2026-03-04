364e03ab2f0ddee6630aff401768e299163357d5
igormanfroi-cpu <igor_manfroi@estudante.sesisenai.org.br>
2026-03-04 20:16:49 +0000

    update

--- atividade python/atividade5.py
+++ atividade python/atividade5.py
@@ -3,5 +3,6 @@ idade = int(input("insira a idade: "))
 
 #repete quando a idade for valida
 while idade > 120 or idade < 0:
-print = int(input("idade(anos comletos - ate 120 anos): "))
+    print = int(input("idade(anos comletos - ate 120 anos): "))
 dias_vida = idade * 365
+print (f"{nome},voce viveu {dias_vida}")
